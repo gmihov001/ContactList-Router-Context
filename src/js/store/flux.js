5b065ce3d6fa0a36db15ce33ef4a310de55a8cef
George Mihov <gmihov001@gmail.com>
2020-08-11 17:18:45 +0000

    Updates

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,9 +1,30 @@
 const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
-			//Your data structures, A.K.A Entities
+			contact: [],
+			agenda: []
 		},
 		actions: {
+			addContact: (name, address, phone, email) => {
+				fetch("https://assets.breatheco.de/apis/fake/contact/", {
+					method: "POST",
+					headers: { "Content-Type": "application/json" },
+					body: JSON.stringify({
+						agenda_slug: "george_agenda",
+						full_name: name,
+						email: email,
+						phone: phone,
+						address: address
+					})
+				})
+					.then(response => response.json())
+					.then(() => {
+						fetch("https://assets.breatheco.de/apis/fake/contact/agenda/george_agenda")
+							.then(response => response.json())
+							.then(data => setStore({ agenda: data }));
+					});
+			}
+
 			//(Arrow) Functions that update the Store
 			// Remember to use the scope: scope.state.store & scope.setState()
 		}
